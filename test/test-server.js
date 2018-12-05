673ad34d2efca509ed449a6c70a67880708ec11c
Gianina Skarlett <skarlett.gianina@gmail.com>
2018-12-05 15:18:01 -0800

    activated runServer

--- test/test-server.js
+++ test/test-server.js
@@ -7,3 +7,12 @@ const expect = chai.expect;
 
 chai.use(chaiHttp);
 
+describe("", function() {
+    // Before our tests run, we activate the server. Our `runServer`
+    // function returns a promise, and we return the that promise by
+    // doing `return runServer`. If we didn't return a promise here,
+    // there's a possibility of a race condition where our tests start
+    // running before our server has started.
+    before(function() {
+      return runServer();
+    });
