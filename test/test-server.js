ebcecfb865087ea11e25a358889401dceb37b0d1
Gianina Skarlett <skarlett.gianina@gmail.com>
2018-12-05 15:29:16 -0800

    closed server after test

--- test/test-server.js
+++ test/test-server.js
@@ -16,3 +16,12 @@ describe("", function() {
     before(function() {
       return runServer();
     });
+
+    // although we only have one test module at the moment, we'll
+    // close our server at the end of these tests. Otherwise,
+    // if we add another test module that also has a `before` block
+    // that starts our server, it will cause an error because the
+    // server would still be running from the previous tests.
+    after(function() {
+        return closeServer();
+    });
