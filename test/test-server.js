86f9ac1ab7b1f4b84536c432d5e66d24c312d501
Gianina Skarlett <skarlett.gianina@gmail.com>
2018-12-05 15:57:54 -0800

    added test for post

--- test/test-server.js
+++ test/test-server.js
@@ -8,29 +8,29 @@ const expect = chai.expect;
 chai.use(chaiHttp);
 
 describe("", function() {
-    // Before our tests run, we activate the server. Our `runServer`
-    // function returns a promise, and we return the that promise by
-    // doing `return runServer`. If we didn't return a promise here,
-    // there's a possibility of a race condition where our tests start
-    // running before our server has started.
-    before(function() {
-      return runServer();
-    });
+  // Before our tests run, we activate the server. Our `runServer`
+  // function returns a promise, and we return the that promise by
+  // doing `return runServer`. If we didn't return a promise here,
+  // there's a possibility of a race condition where our tests start
+  // running before our server has started.
+  before(function() {
+    return runServer();
+  });
 
-    // although we only have one test module at the moment, we'll
-    // close our server at the end of these tests. Otherwise,
-    // if we add another test module that also has a `before` block
-    // that starts our server, it will cause an error because the
-    // server would still be running from the previous tests.
-    after(function() {
-        return closeServer();
-    });
-
-    // test strategy:
-    //  1. make request to `/blog-posts`
-    //  2. inspect response object and prove has right code and have
-    //  right keys in response object.
-  it("it should list blog posts on get", function() {
+  // although we only have one test module at the moment, we'll
+  // close our server at the end of these tests. Otherwise,
+  // if we add another test module that also has a `before` block
+  // that starts our server, it will cause an error because the
+  // server would still be running from the previous tests.
+  after(function() {
+      return closeServer();
+	});
+	
+	// test strategy:
+  //   1. make request to `/shopping-list`
+  //   2. inspect response object and prove has right code and have
+  //   right keys in response object.
+  it("should list items on GET", function() {
     // for Mocha tests, when we're dealing with asynchronous operations,
     // we must either return a Promise object or else call a `done` callback
     // at the end of the test. The `chai.request(server).get...` call is asynchronous
@@ -43,13 +43,37 @@ describe("", function() {
         expect(res).to.be.json;
         expect(res.body).to.be.a("array");
 
+        // because we create three items on app load
         expect(res.body.length).to.be.at.least(1);
         // each item should be an object with key/value pairs
-        // for `title`, `author`, `content`
-        const expectedKeys = ["title", "author", "content"];
+        const expectedKeys = ["title", "id", "author", "content"];
         res.body.forEach(function(item) {
           expect(item).to.be.a("object");
           expect(item).to.include.keys(expectedKeys);
         });
+      });
+  });
+
+	 // test strategy:
+  //  1. make a POST request with data for a new item
+  //  2. inspect response object and prove it has right
+  //  status code and that the returned object has an `id`
+  it("should add an item on POST", function() {
+    const newItem = { title "Juan", author: "paulo", content: "The life of Juan"};
+    return chai
+      .request(app)
+      .post("/blog-posts")
+      .send(newItem)
+      .then(function(res) {
+        expect(res).to.have.status(201);
+        expect(res).to.be.json;
+        expect(res.body).to.be.a("object");
+        expect(res.body).to.include.keys("id", "title", "author", "content");
+        expect(res.body.id).to.not.equal(null);
+        // response should be deep equal to `newItem` from above if we assign
+        // `id` to it from `res.body.id`
+        expect(res.body).to.deep.equal(
+          Object.assign(newItem, { id: res.body.id })
+        );
       });
   });
