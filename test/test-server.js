3ef7f8e77a565c61b0f3dd6bcfd28a5e067afd59
Gianina Skarlett <skarlett.gianina@gmail.com>
2018-12-05 16:02:11 -0800

    added test for put

--- test/test-server.js
+++ test/test-server.js
@@ -76,4 +76,50 @@ describe("", function() {
           Object.assign(newItem, { id: res.body.id })
         );
       });
+	});
+	
+  // test strategy:
+  //  1. initialize some update data (we won't have an `id` yet)
+  //  2. make a GET request so we can get an item to update
+  //  3. add the `id` to `updateData`
+  //  4. Make a PUT request with `updateData`
+  //  5. Inspect the response object to ensure it
+  //  has right status code and that we get back an updated
+  //  item with the right data in it.
+  it("should update items on PUT", function() {
+    // we initialize our updateData here and then after the initial
+    // request to the app, we update it with an `id` property so
+    // we can make a second, PUT call to the app.
+    const updateData = {
+      title: "bake a coffee cake",
+			author: "John mcmuffin"
+			content: "This is how you bake an awesome cake..."
+    };
+
+    return (
+      chai
+        .request(app)
+        // first have to get so we have an idea of object to update
+        .get("/blog-posts")
+        .then(function(res) {
+          updateData.id = res.body[0].id;
+          // this will return a promise whose value will be the response
+          // object, which we can inspect in the next `then` block. Note
+          // that we could have used a nested callback here instead of
+          // returning a promise and chaining with `then`, but we find
+          // this approach cleaner and easier to read and reason about.
+          return chai
+            .request(app)
+            .put(`/blog-posts/${updateData.id}`)
+            .send(updateData);
+        })
+        // prove that the PUT request has right status code
+        // and returns updated item
+        .then(function(res) {
+          expect(res).to.have.status(200);
+          expect(res).to.be.json;
+          expect(res.body).to.be.a("object");
+          expect(res.body).to.deep.equal(updateData);
+        })
+    );
   });
